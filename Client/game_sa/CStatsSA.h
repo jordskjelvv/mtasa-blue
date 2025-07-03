1debe50c04b2277618f72fe9acfbfad6c59529f1
Oleh Ladaniuk <164164741+jordskjelvv@users.noreply.github.com>
2025-07-03 16:04:35 +0300

    Update CStatsSA.h

--- Client/game_sa/CStatsSA.h
+++ Client/game_sa/CStatsSA.h
@@ -21,9 +21,9 @@
 class CStatsSA : public CStats
 {
 public:
-    float GetStatValue(unsigned short usIndex);
-    void  ModifyStat(unsigned short usIndex, float fAmmount);
-    void  SetStatValue(unsigned short usIndex, float fAmmount);
+    float GetStatValue(std::uint32_t usIndex);
+    void  ModifyStat(std::uint32_t usIndex, float fAmmount);
+    void  SetStatValue(std::uint32_t usIndex, float fAmmount);
 
-    unsigned short GetSkillStatIndex(eWeaponType type);
+    std::uint32_t GetSkillStatIndex(eWeaponType type);
 };
