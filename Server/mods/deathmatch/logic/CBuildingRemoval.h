bf5712a55fc19e020bcaf274c1f6d86b37cce0bd
Oleh Ladaniuk <164164741+jordskjelvv@users.noreply.github.com>
2025-07-08 15:55:38 +0300

    Update CBuildingRemoval.h

--- Server/mods/deathmatch/logic/CBuildingRemoval.h
+++ Server/mods/deathmatch/logic/CBuildingRemoval.h
@@ -18,11 +18,11 @@ public:
 
     float          GetRadius() { return m_fRadius; }
     const CVector& GetPosition() { return m_vecPos; }
-    std::uint32_t GetModel() { return m_usModel; }
+    std::uint32_t  GetModel() { return m_usModel; }
     char           GetInterior() { return m_cInterior; }
 
 private:
-    unsigned short m_usModel;
+    std::uint32_t  m_usModel;
     CVector        m_vecPos;
     float          m_fRadius;
     char           m_cInterior;
